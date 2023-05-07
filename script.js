5911592d1faae6dee228573b8efb4092c7168138
Damon <126731021+damon314159@users.noreply.github.com>
2023-05-07 14:05:44 +0100

    Add functionality to the grid size selectors
    Creates function intializeGrids() that replaces the grids from a stored
    array as requested by the user clicking the respective buttons. This
    should allow the drawings to be saved when changing size

--- script.js
+++ script.js
@@ -3,18 +3,19 @@
 function createGrid (size) {
     let array=[];
     const grid = document.createElement("div");
-    const dimensions = 600/size;
+    grid.classList.add("grid");
+    const dimensions = 600/size; //height and width of cells
 
-    for (let i=0; i<size; i++){
+    for (let i=0; i<size; i++){  //define rows
         array[i] = document.createElement("div");
         array[i].classList.add("row");
         array[i].setAttribute("style", 
         "width:"+600+"px;"
         + "height:"+dimensions+"px;"
         + "display:flex;");
-
         grid.appendChild(array[i]);
-        for (let j=0; j<size; j++) {
+    
+        for (let j=0; j<size; j++) { //define cells in each row
             array[i][j] = document.createElement("div");
             array[i][j].classList.add("grid-element");
             array[i][j].setAttribute("style", 
@@ -27,7 +28,35 @@ function createGrid (size) {
     return grid;
 };
 
-
+function initializeGrids() {
+    const sizeBtnList = document.querySelectorAll(".size-buttons>*")
+    const tenGrid = createGrid(10);
+    const twentyGrid = createGrid(20);
+    const thirtyGrid = createGrid(30);
+    let gridsArray = [tenGrid, twentyGrid, thirtyGrid];
+    canvas.appendChild(gridsArray[0]);
+    sizeBtnList.forEach(element => {
+        element.addEventListener("click", (event)=> {
+            const size = event.target.classList[0].slice(-2); //eg button.sqr10 -> 10
+            canvas.removeChild(canvas.children[0]);
+            switch (size) {
+                case "10":
+                    canvas.appendChild(gridsArray[0])
+                    break;
+                case "20":
+                    canvas.appendChild(gridsArray[1])
+                    break;
+                case "30":
+                    canvas.appendChild(gridsArray[2])
+                    break;
+                default:
+                    console.log("something went wrong");
+            };
+            
+        });
+    });
+};
+                
+                
 const canvas = document.querySelector(".canvas");
-const tenGrid = createGrid(10);
-canvas.appendChild(tenGrid);
+initializeGrids();
